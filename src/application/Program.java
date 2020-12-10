ceeee1204960ab4654fef3eae62beaf5250b64a2
Antonio Júnior <jurniores@gmail.com>
2020-12-10 21:30:54 -0200

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -6,7 +6,9 @@
 package application;
 
 import java.util.Date;
-import java.text.SimpleDateFormat;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -29,7 +31,7 @@ public class Program {
         Department obj = new Department(1,"Books");
         Seller seller = new Seller(21,"Bob","bob@gmail.com",new Date(),3000.0,obj);
         System.out.println(seller.getDepartment().getId());
-        
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         
         }
        
