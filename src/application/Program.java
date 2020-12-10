17d8d83e0fed97e6bdbe14d4f26b58ab4a3306c4
Antonio Júnior <jurniores@gmail.com>
2020-12-10 20:40:40 -0200

    Department class

---
+++ src/application/Program.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import db.DB;
+import db.DbException;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import model.entities.Department;
+
+
+
+
+
+/**
+ *
+ * @author Antonio
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Department obj = new Department(1,"Books");
+        
+        System.out.println(obj);
+        
+        
+        }
+       
+    }
+    
